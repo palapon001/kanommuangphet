2c78c55cf2d0fedc5c377cb8f40840fbb24b577c
Mon <palapon@thaiware.com>
2025-07-04 16:22:53 +0700

    feat: add Kanit font integration and apply it globally in header_footer.php

--- src/header_footer.php
+++ src/header_footer.php
@@ -42,6 +42,13 @@ function renderHead($config, $pageType = 'main') // default เป็น main
         <script src="<?= $config['url'] ?>assets/vendor/js/helpers.js"></script>
         <script src="<?= $config['url'] ?>assets/js/config.js"></script>
 
+        <!-- fontKanit -->
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link
+            href="https://fonts.googleapis.com/css2?family=Kanit:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+            rel="stylesheet">
+
         <!-- Font Awesome CDN -->
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css"
             integrity="sha512-..." crossorigin="anonymous" referrerpolicy="no-referrer" />
@@ -73,6 +80,10 @@ function renderHead($config, $pageType = 'main') // default เป็น main
         <!-- sweetalert2 -->
         <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
         <style>
+            * {
+                font-family: 'Kanit', sans-serif;
+            }
+
             .my-swal-container {
                 z-index: 1100 !important;
             }
